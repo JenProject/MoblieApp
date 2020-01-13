19eda2ab9333aa2569df4ecc91da8172e1b312a5
edw1nsky <57032814+edw1nsky@users.noreply.github.com>
2020-01-13 22:29:33 +0100

    Add files via upload

--- BoardingChosen.java
+++ BoardingChosen.java
@@ -3,36 +3,31 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class CategoryChosen1 extends AppCompatActivity {
+public class BoardingChosen extends AppCompatActivity {
 
     ImageButton btnBackToMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category_chosen1);
+        setContentView(R.layout.activity_boarding_chosen);
 
-        String Message = "TRANSPORT";
+        String Message = "BOARDING";
         Toast.makeText(this, Message, Toast.LENGTH_SHORT).show();
 
         btnBackToMenu = (ImageButton) findViewById(R.id.btn_BackToMenu);
         btnBackToMenu.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(CategoryChosen1.this, MainActivity.class);
+                Intent intent = new Intent(BoardingChosen.this, MainActivity.class);
                 startActivity(intent);
 
             }
         });
     }
-
-
 }
-
--- MainActivity.java
+++ MainActivity.java
@@ -10,13 +10,19 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton btnChooseTransport;
+    ImageButton btnChooseTransport, btnChooseBoarding, btnChooseFood, btnChooseMedicine, btnChooseShopping, btnChooseSightsee ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnChooseTransport = (ImageButton) findViewById(R.id.btn_ChooseTransport);
+        btnChooseBoarding = (ImageButton) findViewById(R.id.btn_ChooseBoarding);
+        btnChooseFood = (ImageButton) findViewById(R.id.btn_ChooseFood);
+        btnChooseMedicine = (ImageButton) findViewById(R.id.btn_ChooseMedicine);
+        btnChooseShopping = (ImageButton) findViewById(R.id.btn_ChooseShopping);
+        btnChooseSightsee = (ImageButton) findViewById(R.id.btn_ChooseSightsee);
+
         btnChooseTransport.setOnClickListener(new View.OnClickListener(){
             @Override
                     public void onClick(View v){
@@ -25,5 +31,55 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        btnChooseBoarding.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, BoardingChosen.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnChooseFood.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, FoodChosen.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnChooseMedicine.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, MedicChosen.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnChooseShopping.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, ShoppingChosen.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnChooseSightsee.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, SightseeChosen.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        }
+
+
     }
-}
+
